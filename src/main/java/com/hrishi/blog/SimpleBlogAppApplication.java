2541273df4b78c6eb432ca7bc890702df7dce67d
desktop.hrishikesh <itishrishikesh@gmail.com>
2024-03-01 17:39:26 +0530

    spring boot project initialized

---
+++ src/main/java/com/hrishi/blog/SimpleBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.hrishi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrishi/blog/SimpleBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishi.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
